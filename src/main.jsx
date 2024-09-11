db27bfbd960e6e36453689aa92d808b0aaa569b5
Ignas <ignas.petrausk@gmail.com>
2024-09-11 21:39:58 +0300

    routes edited 2

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
     element: <Home />,
   },
   {
-    path: '/geo-quiz/find-countries',
+    path: '/find-countries',
     element: <FindCountries />,
   },
 ]);
